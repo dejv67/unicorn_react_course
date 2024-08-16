b3945a300ec23f278534dc5d33c69f7409e267a1
David Kral <david.kral.2@unicorn.com>
2024-08-16 10:11:05 +0200

    4.task
    configured calling backend for data

--- cookbook/client/src/App.js
+++ cookbook/client/src/App.js
@@ -1,11 +1,56 @@
-import './App.css';
+import { useState, useEffect } from "react";
+import styles from './App.css';
 import data from './bricks/mockup';
 import RecipeList from './bricks/RecipeList';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Icon } from '@mdi/react';  // Ensure you use a named import
-import { mdiBookOpenVariant } from '@mdi/js';
+import { mdiBookOpenVariant, mdiLoading } from '@mdi/js';
 
 function App() {
+    const [recipeListCall, setRecipeListCall] = useState({
+        state: "pending",
+    });
+
+    useEffect(() => {
+        fetch(`http://localhost:3000/recipe/list`, {
+            method: "GET",
+        }).then(async (response) => {
+            const responseJson = await response.json();
+            if (response.status >= 400) {
+                setRecipeListCall({ state: "error", error: responseJson });
+            } else {
+                setRecipeListCall({ state: "success", data: responseJson });
+            }
+        });
+    }, []);
+
+    function getRecipes() {
+        switch (recipeListCall.state) {
+            case "pending":
+                return (
+                    <div className={styles.loading}>
+                        <Icon size={2} path={mdiLoading} spin={true} />
+                    </div>
+                );
+            case "success":
+                return (
+                    <>
+                        <RecipeList recipeList={recipeListCall.data} />
+                    </>
+                );
+            case "error":
+                return (
+                    <div className={styles.error}>
+                        <div>Nepodařilo se načíst data o třídě.</div>
+                        <br />
+                        <pre>{JSON.stringify(recipeListCall.error, null, 2)}</pre>
+                    </div>
+                );
+            default:
+                return null;
+        }
+    }
+
     return (
         <div className="App">
             {/* Add className to Icon for styling */}
@@ -13,7 +58,7 @@ function App() {
                 <Icon className="mdi" path={mdiBookOpenVariant} size={2} color="white" />
                 David’s Cookbook
             </h1>
-            <RecipeList recipeList={data} />
+            {getRecipes()}
         </div>
     );
 }
